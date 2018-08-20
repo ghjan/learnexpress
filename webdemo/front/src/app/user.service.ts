cda6932c9dfcc31d05ac7108a31ba4bec70098ed
jian.zhang <cajan2@163.com>
2018-08-20 13:53:35 +0800

    save

--- webdemo/front/src/app/user.service.ts
+++ webdemo/front/src/app/user.service.ts
@@ -38,8 +38,10 @@ export class UserService {
   // 错误处理
   handleError(err: HttpErrorResponse): string {
     if (err.error instanceof Error) {
+      // 如果在客户端这边出了错误（比如在RxJS操作符中抛出的异常或某些阻碍完成这个请求的网络错误），就会抛出一个Error类型的异常
       return '发生错误，错误信息:' + err.error.message;
     } else {
+      // 后端返回了一个失败的返回码（如404、500等）
       console.log(`Backend returned code ${err.status}, body was: ${err.error['msg']}`);
       return err.error['msg'];
     }
